e167c1030dd9861cbc9b7afc2ab62c9d039f7989
zjl <1536654227@qq.com>
2021-08-03 20:37:34 +0800

    second

--- src/App.vue
+++ src/App.vue
@@ -7,7 +7,6 @@
         <code v-text="'<el-button>'"></code>
         below
       </p>
-      <el-button>el-button</el-button>
     </div>
     <HelloWorld msg="Welcome to Your Vue.js App"/>
   </div>
--- src/plugins/element.js
+++ src/plugins/element.js
@@ -1,4 +1,4 @@
-import Vue from 'vue'
+import { createApp } from 'vue'
 import { Button } from 'element-ui'
 
 Vue.use(Button)
