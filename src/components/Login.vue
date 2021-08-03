f4364c8262c7cb655c4e47d55c0c24e9ec7eb610
zjl <1536654227@qq.com>
2021-08-03 17:16:55 +0800

    second

---
+++ src/components/Login.vue
@@ -0,0 +1,74 @@
+<template>
+  <div class="login_container">
+    <div class="login_box">
+      <div class="avatar_box">
+        <img src="../assets/logo.png" alt="" />
+      </div>
+
+      <el-form label-width="80px">
+        <el-form-item label="活动名称">
+          <el-input></el-input>
+        </el-form-item>
+      </el-form>
+    </div>
+  </div>
+</template>
+
+<script>
+export default {
+  components: {},
+  data() {
+    return {};
+  },
+  //监听属性
+  computed: {},
+  //监控data中的数据变化
+  watch: {},
+  methods: {},
+  created() {},
+  mounted() {},
+  beforeCreate() {},
+  beforeMount() {},
+  beforeUpdate() {},
+  updated() {},
+  beforeUnmounted() {},
+  unmounted() {},
+  activated() {}, //如果页面有keep-alive缓存功能，这个函数会触发
+};
+</script>
+<style scoped>
+.login_container {
+  background-color: #2b4b6b;
+  height: 100%;
+}
+.login_box {
+  height: 300px;
+  width: 450px;
+  background-color: #fff;
+  border-radius: 3px;
+  position: relative;
+  left: 50%;
+  top: 50%;
+  transform: translateX(-50%) translateY(-50%);
+}
+.avatar_box {
+  padding: 10px;
+  width: 130px;
+  height: 130px;
+  background-color: #fff;
+  border: 1px solid #eee;
+  border-radius: 50%;
+  box-shadow: 0 0 10px #ddd;
+  position: absolute;
+  left: 50%;
+  transform: translateX(-50%);
+  top: -25%;
+}
+.login_box .avatar_box img {
+  width: 130px;
+  height: 130px;
+  border: 1px solid #eee;
+  border-radius: 50%;
+  background-color: #eee;
+}
+</style>
